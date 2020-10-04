dcb18d0640205f6ad59bc3d73424e4f8b8d92896
Ioan <ioan.sava28@gmail.com>
2020-10-04 21:53:29 +0300

    project: Hello World AR

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Distance;
+    private float CLOSE_DISTANCE = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Distance = Vector3.Distance(Camera.main.transform.position, transform.position);
+        if (Distance < CLOSE_DISTANCE)
+        {
+            GetComponent<Animator>().SetBool("isCameraClose", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("isCameraClose", false);
+        }
+    }
+}
